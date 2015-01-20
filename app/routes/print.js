85554e4047d6ca90e941eb3059b47333232e9f71
Henrik Steen <henrist@henrist.net>
2015-01-20 04:58:56 +0100

    Check if printer is offline when printing

--- app/routes/print.js
+++ app/routes/print.js
@@ -11,23 +11,40 @@ router.post('/', function(req, res, next) {
     return;
   }
 
-  // print the file
-  execFile('lp', ['-d', 'SEWOO_LKT_Series', req.files['file'].path], function (error, stdout, stderr) {
-    if (error !== null) {
-      res.status(500).json({
-        status: 'Print command failed',
-        code: error.code,
-        stderr: stderr
-      });
+  checkQueue();
 
-      return;
-    }
+  function checkQueue() {
+    execFile('lpstat', ['-l'], function (error, stdout, stderr) {
+      if (stdout.indexOf('Waiting for printer to become available') !== -1) {
+        res.status(503).json({
+          status: 'Printer offline',
+          code: 1,
+          stderr: 'OFFLINE'
+        });
+        return;
+      }
 
-    res.json({
-      status: 'Print OK',
-      stdout: stdout
+      printFile();
     });
-  });
+  }
+
+  function printFile() {
+    execFile('lp', ['-d', 'SEWOO_LKT_Series', req.files['file'].path], function (error, stdout, stderr) {
+      if (error !== null) {
+        res.status(500).json({
+          status: 'Print command failed',
+          code: error.code,
+          stderr: stderr
+        });
+        return;
+      }
+
+      res.json({
+        status: 'Print OK',
+        stdout: stdout
+      });
+    });
+  }
 });
 
 module.exports = router;
